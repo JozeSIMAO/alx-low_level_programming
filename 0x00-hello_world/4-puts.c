dd108bf69575f69e03f6408072475f209f4d044c
JozeSIMAO <jozesimao10@gmail.com>
2023-02-16 04:18:43 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 int main(void)
 {
-  char Mystr1[]="\"Programming is like building a multilingual puzzle";
+  char Mystr1[] = "\"Programming is like building a multilingual puzzle";
   
   puts(Mystr1);
-  return 0;
   
 }
