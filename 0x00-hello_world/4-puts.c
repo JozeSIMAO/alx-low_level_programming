1dccfee718309a22599586dc361ab1fc7ba3a3ef
JozeSIMAO <jozesimao10@gmail.com>
2023-02-16 04:32:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-char Mystr1[]= "\"Programming is like building a multilingual puzzle";  
+char Mystr1[] ="\"Programming is like building a multilingual puzzle";  
 puts(Mystr1);
 
- return (0);
+return (0);
 }
