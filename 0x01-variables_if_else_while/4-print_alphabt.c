64fdddbdabb3266e0b6fd9e374748050a64c60cc
JozeSIMAO <jozesimao10@gmail.com>
2023-02-17 07:34:33 -0800

    alphabts

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints alphabets in lowercase except q and e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+if(lc != 'q' && lc != 'e')
+{
+putchar(lc);
+}
+}
+putchar('\n');
+
+return (0);
+}
