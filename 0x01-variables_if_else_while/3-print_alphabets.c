10c247a1507036001c58ca51ed2f85a9718f5e4a
JozeSIMAO <jozesimao10@gmail.com>
2023-02-17 07:11:33 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry point
+ *
+ * Description: 'prints alphabets in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lc;
+char lb;
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+putchar(lc);
+}
+for (lb = 'A'; lb <= 'Z'; lb++)
+}
+putchar(lb);
+}
+putchar('\n');
+
+return (0);
+}
+
