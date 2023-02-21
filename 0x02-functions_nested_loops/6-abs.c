584f59fe2ba63d487c9b3862e74600d2c92244fd
JozeSIMAO <jozesimao10@gmail.com>
2023-02-21 10:41:35 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - gives absolute value of integer
+ * @n: is to be checked
+ * Return: Always (0) success
+ */
+int _abs(int n)
+{
+if ( n >= 0)
+return (n);
+else
+return (-n);
+}
