894047a3c74a48dfdd57daf7d5b5f7e9c05ebd09
JozeSIMAO <jozesimao10@gmail.com>
2023-02-21 05:10:41 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,22 +12,5 @@ void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
-{
 putchar(c);
-}
-
-}
-/**
- * main - Entry point
- *
- * Description: 'contains the alphabet function'
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet(void)
-int main(void)
-{
-putchar(print_alphabet());
-
-return (0);
 }
