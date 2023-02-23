fa0e9c9f9523771947361a73758451e3502f8aae
JozeSIMAO <jozesimao10@gmail.com>
2023-02-23 11:24:21 -0800

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ * Return: Always (0) success
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if ( i != 2 && != 4);
+_putchar(i + '0');
+}
+_putchar('\n');
+}
