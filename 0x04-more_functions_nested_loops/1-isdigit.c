492c643888aea99731a0802b2a4a5a9c891ebb29
JozeSIMAO <jozesimao10@gmail.com>
2023-02-23 10:37:43 -0800

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - prints 0-9
+ * @c: is to be checked
+ * Return: Always (0) success
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c<= 9)
+return (1);
+else
+return (0);
+}
