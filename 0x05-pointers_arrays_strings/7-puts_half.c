5a3210170bb9bd71a3e7153b5b75ed7507755fb2
JozeSIMAO <jozesimao10@gmail.com>
2023-02-27 08:51:31 -0800

    task7_half_puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,25 +10,18 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int n;
-	int len = 0;
 
-	while (str[len] != '\0')
+	while (str[i] != '\0')
 	{
-		len++;
+		i += 1;
+		m = i / 2;
 	}
-	if (len % 2 == 0)
+	if ( i % 2 == 1)
+		n += 1;
+	while (str[n] != '\0')
 	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else if (len % 2)
-	{
-		for (n = (len - 1) / 2; i < len - 1; i++)
-		{
-			_putchar(str[n + 1]);
-		}
+		_putchar(*(str + n));
+		n++;
 	}
 	_putchar('\n');
 }
