2e070f2e8214fa1e8fea685d0fe128790317960a
JozeSIMAO <jozesimao10@gmail.com>
2023-02-27 03:27:51 -0800

    task3_put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str : is the string
+ * Return : Always (0) success
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] != '\0')
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
