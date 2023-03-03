89580de01a8bf00a499dc5cd3f2366e79e67ec03
JozeSIMAO <jozesimao10@gmail.com>
2023-03-03 03:56:49 -0800

    task7_leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string
+ * @str: string
+ *
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10: j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
