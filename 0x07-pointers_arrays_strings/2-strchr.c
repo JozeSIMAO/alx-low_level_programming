6850c2763700b6274139b86327042626d960226a
JozeSIMAO <jozesimao10@gmail.com>
2023-03-06 04:27:38 -0800

    task2_strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return ((char *) s);
+			return (&s[i]);
 		}
-		s++;
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
