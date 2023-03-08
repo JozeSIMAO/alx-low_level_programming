662566201d94e6f6f76b2dc605efa81871de9433
JozeSIMAO <jozesimao10@gmail.com>
2023-03-08 04:19:35 -0800

    task0_puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i])
+	if (*s == 0)
 	{
-		_putchar(s[i]);
-		i++;
+		return;
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
