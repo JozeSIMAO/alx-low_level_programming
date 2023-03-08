7699a9482a79b229115c9c50a142864e7452e066
JozeSIMAO <jozesimao10@gmail.com>
2023-03-08 10:04:59 -0800

    task5_sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,14 +8,19 @@
 
 int square(int n, int val)
 {
-        if (val * val == n)
-                return (val);
-        else if (val * val < n)
-                return (square(n, val + 1));
-        else
-                return (-1);
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (square(n, val + 1));
+	}
+	else
+	{
+		return (-1);
+	}
 }
-
 
 /**
  * int _sqrt_recursion - returns natural square root a numb
