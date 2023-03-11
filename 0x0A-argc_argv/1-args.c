deacb5c9801d36be2d510328b345e60115344a59
JozeSIMAO <jozesimao10@gmail.com>
2023-03-11 08:01:14 -0800

    task1_args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: input
+ * @argv: input
+ * Return: Always (0) success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
