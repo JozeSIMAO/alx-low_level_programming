d02327b3ce81f7e8e0abb070dbaffb8b36f0f720
JozeSIMAO <jozesimao10@gmail.com>
2023-03-13 07:38:25 -0700

    task100_argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int ac, char **av)
 			len++;
 		len++;
 	}
-	str = malloc(sizeof(char) * (len));
+	str = (char *) malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
