003443971f927117a748eaa1c6e8adaf52941bc0
JozeSIMAO <jozesimao10@gmail.com>
2023-03-14 14:53:28 -0700

    task100_argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * argstostr - concatenates all arguments
- * @ac: number of arguments
- * @av: array of arguments
- * Return: s
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
+
+	int i, n, r = 0, l = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			len++;
+		for (n = 0; av[i][n]; n++)
+			l++;
 	}
-	len += ac;
+	l += ac;
 
-	str = (char *) malloc(sizeof(char) * (len + 1));
-
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
-
-	k = 0;
-
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		if (str[k] == '\0')
-		{
-		str[k++] = '\n';
-		}
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
 	}
 	return (str);
 }
