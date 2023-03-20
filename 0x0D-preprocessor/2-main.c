1e2d1713f3c8ca0d75f24ab78d4159c135c61d32
JozeSIMAO <jozesimao10@gmail.com>
2023-03-20 07:14:59 -0700

    task2_fileName

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file its compiled on
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: name
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
