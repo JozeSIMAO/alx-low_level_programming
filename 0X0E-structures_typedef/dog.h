4e23faa7b723128a3e1ffb6f9925ef50c7400e06
JozeSIMAO <jozesimao10@gmail.com>
2023-03-20 08:49:47 -0700

    task0_dog_h

---
+++ 0X0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - struct for dogs
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: its for dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
---
+++ 0X0E-structures_typedef/main.h
@@ -0,0 +1,11 @@
+#define main_h
+#ifndef main_h
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#define dog_h
+#define dog_t
+
+#endif
