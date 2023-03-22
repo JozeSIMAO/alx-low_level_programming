f5a7e4a20ccbc5e4b730937e2f03cd1b8133e764
JozeSIMAO <jozesimao10@gmail.com>
2023-03-22 04:03:24 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - prints name
  * @name: is namae
@@ -9,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	(*f)(name);
 }
