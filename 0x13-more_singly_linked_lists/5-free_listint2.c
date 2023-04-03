d2db92b1f682a60d30402a38d50449d84d2c8c4b
JozeSIMAO <jozesimao10@gmail.com>
2023-04-03 06:55:21 -0700

    task5_free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: double pointer to head node
+ * Return: 0;
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+	{
+		return;
+	}
+	else
+	{
+		listint_t *temp;
+		listint_t *temp1 = *head;
+
+		while (temp1)
+		{
+			temp = temp1->next;
+			free(temp1);
+			temp1 = temp;
+		}
+	}
+	*head = NULL;
+}
