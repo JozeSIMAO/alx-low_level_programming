7e226930e4c9d764d4608df1360aec31948a1807
JozeSIMAO <jozesimao10@gmail.com>
2023-04-04 04:56:06 -0700

    task5_free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,7 @@ void free_listint2(listint_t **head)
 {
 	if (*head == NULL)
 	{
+		free(*head);
 		return;
 	}
 	else
@@ -27,5 +28,4 @@ void free_listint2(listint_t **head)
 	}
 
 	*head = NULL;
-	free(*head);
 }
