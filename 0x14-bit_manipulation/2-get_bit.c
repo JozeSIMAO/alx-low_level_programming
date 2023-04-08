174eb9baf81defcfcaca3891741a418af14cd556
JozeSIMAO <jozesimao10@gmail.com>
2023-04-08 07:03:35 -0700

    task2_get_bit_at_index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,26 +8,10 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int *binary = malloc(sizeof(unsigned int) * 32);
-	int indx = 0, result;
-
-	if (binary == NULL)
-		return (-1);
-	if (index >= 32)
+	if (index >= sizeof(unsigned int) * 8)
 	{
-		free(binary);
 		return (-1);
 	}
 
-	while (n > 0)
-	{
-		binary[indx] = n % 2;
-		n /= 2;
-		indx++;
-	}
-
-	result = binary[index];
-	free(binary);
-
-	return (result);
+	return ((n >> index) & 1);
 }
