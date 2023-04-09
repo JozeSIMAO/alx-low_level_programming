a92630834a5a169735738880fd656aedf02436b8
JozeSIMAO <jozesimao10@gmail.com>
2023-04-09 08:35:39 -0700

    task0_read_text_file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file ands prints it to the POSIX STNDOUT
+ * @filname: is pointer to a file
+ * @letters if the number of letters to be read and printed
+ * Return: num of letter
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE* output;
+	char *chr;
+	ssize_t read, written;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	output = fopen(filename, "r");
+
+	if (output == NULL)
+	{
+		return (0);
+	}
+
+	chr = malloc(letters + 1);
+	if (chr == NULL)
+	{
+		fclose(output);
+		return (0);
+	}
+
+	read = fread(chr, sizeof(char), letters, output);
+	if (read <= 0)
+	{
+		fclose(output);
+		free(chr);
+		return (0);
+	}
+
+	written = write(STDOUT_FILENO, chr, read);
+	if (written < read)
+	{
+		fclose(output);
+		free(chr);
+		return (0);
+	}
+	else
+	{
+		fclose(output);
+		free(chr);
+		return (written);
+	}
+}
