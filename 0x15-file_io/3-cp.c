ceda939601d46d8988c0be9525e952c9628966f4
JozeSIMAO <jozesimao10@gmail.com>
2023-04-10 16:55:46 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,20 +12,26 @@ int main(int argc, char *argv[])
 	mode_t file_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	to = open(argv[2], O_WRONLY | O_TRUNC | O_CRET, file_mode);
+	}
+	to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, file_mode);
 	if (to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
+	}
 	while ((file_read = read(from, buffer, BUFFER)) > 0)
 	{
 		file_written = write(to, buffer, file_read);
-		if (written != file_read)
+		if (file_written != file_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -33,7 +39,7 @@ int main(int argc, char *argv[])
 	}
 	if (file_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1];
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 				exit(98);
 	}
 	if (close(from) == -1)
