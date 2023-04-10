aa30f47854d87036c01e17e69a9b65bb94641857
JozeSIMAO <jozesimao10@gmail.com>
2023-04-10 11:05:30 -0700

    task2_append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,14 +29,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	int written;
 
 	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		return (-1);
+		return (-1)
 
 	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (file == -1)
 		return (-1);
+
 	written = write(file, text_content, _strlen(text_content));
 
 	if (written == -1 || written != _strlen(text_content))
