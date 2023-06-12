41b3b833413d770d2febc56a95b842875167e8ea
JozeSIMAO <jozesimao10@gmail.com>
2023-06-12 10:42:42 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,57 @@
 #include "main.h"
 /**
+ * str_len - gets the length of a string
+ * @str: is string
+ * Return: length
+ */
+int str_len(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
  * str_concat - concatenates two strings
  * @s1: first string
  * @s2: s2nd string
  * Return: pointer to new string
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *newStr;
-	int i = 0, j = 0, k = 0;
-	int len = 0, p = 0;
+	int i = 0, k = 0;
+	int len = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len = str_len(s1) + str_len(s2);
+	newStr = malloc(sizeof(*newStr) * (len + 1));
+
+	if (newStr == 0)
 	{
 		return (NULL);
 	}
 	else
 	{
-		while (s1[i] != '\0')
-		{
-			i++;
-		}
-		while (s2[j] != '\0')
-		{
-			j++;
-		}
-		len = i + j;
-
-		newStr = malloc(sizeof(*newStr) * (len + 1));
-
-		if (newStr == NULL)
-		{
-			return (NULL);
-		}
-		while (k < i)
+		while (k < str_len(s1))
 		{
 			newStr[k] = s1[k];
 			k++;
 		}
-		while (k < len)
+		while (i < str_len(s2))
 		{
-			newStr[k] = s2[p];
+			newStr[k] = s2[i];
+			i++;
 			k++;
-			p++;
 		}
-		newStr[k + 1] = '\0';
+		newStr[k] = '\0';
 		return (newStr);
 	}
 }
