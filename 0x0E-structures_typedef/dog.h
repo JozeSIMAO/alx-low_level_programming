92f4cc90f37727408bbecfde4074eacb96c74888
JozeSIMAO <jozesimao10@gmail.com>
2023-06-19 06:23:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for dogs
+ * @name: is name of dog
+ * @age: if age of the dog
+ * @owner: is the owner of the dog
+ *
+ * Description: This struct is used to create a set of
+ * different dogs and their owners with also their
+ * information
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
