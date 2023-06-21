5480e38970bc82f4ae38f3c82a52b000765c8f5e
JozeSIMAO <jozesimao10@gmail.com>
2023-06-21 05:38:49 -0700

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,25 +4,22 @@
  * @size: number of elements in an array
  * @array: array to search in
  * @cmp: function pointer
- * Return: integer found
+ * Return: index of integer found
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || cmp == NULL)
-		exit(98);
-	if (size <= 0)
-		return (-1);
-
-	while (i < size)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
-		i++;
 	}
 	return (-1);
 }
