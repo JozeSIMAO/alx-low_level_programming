79b4e095d104841254d88e03c099a1ed35f30865
JozeSIMAO <jozesimao10@gmail.com>
2023-07-04 07:15:48 -0700

    bug_fix1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,11 +15,12 @@ void free_listint2(listint_t **head)
 	{
 		return;
 	}
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
 
-
-	temp = (*head)->next;
-	free(*head);
-	*head = temp;
-
-	*head = NULL;
+	head = NULL;
 }
