b606dbc5cf8f2721760e48dd2e94ab86bc90f12a
JozeSIMAO <jozesimao10@gmail.com>
2023-07-06 12:59:06 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -26,14 +26,14 @@ int _strlen(const char *str)
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i = _strlen(b);
+	int i = 0;
 	unsigned int baseVal = 1;
 	unsigned int total = 0;
 
 	if (b == NULL)
-	{
 		return (0);
-	}
+
+	i = _strlen(b);
 
 	for (i = i - 1; i >= 0 && b[i] != '\0'; i--)
 	{
