799c3f3cfd82a78c5fdf46767eea73efe6b2e16d
JozeSIMAO <jozesimao10@gmail.com>
2023-08-17 09:24:32 -0700

    print nodes

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
-	dlistint_t *temp = h
 
-	while (temp)
+	while (h)
 	{
 		counter++;
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
 	return (counter);
