a7406709adfac4ef3e5a6bcc38729a4dab6d6430
JozeSIMAO <jozesimao10@gmail.com>
2023-09-07 14:09:02 -0700

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_table_t *new_table;
 	unsigned long int i = 0;
 
 	if (size == 0)
 		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
@@ -27,6 +29,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	while (i < size)
 	{
 		new_table->array[i] = NULL;
+		i++;
 	}
 
 	return (new_table);
